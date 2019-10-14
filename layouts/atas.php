28092a91cf09330d145e5a5d7176a28802c6603d
Billy-005 <official.billy005@gmail.com>
2019-10-14 22:18:59 +0700

    update fix projek akhir

---
+++ layouts/atas.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title><?php echo $title ?></title>
+	<link rel="stylesheet" type="text/css" href="plugins/font-awesome-4.7.0/css/font-awesome.min.css">
+	<link href="https://fonts.googleapis.com/css?family=Bree+Serif&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+	<?php include "header.php" ?>
---
+++ layouts/header.php
@@ -0,0 +1,19 @@
+<header>
+	<div class="container">
+		<div class="logo">
+			<h1>InheAgriculture <i style="margin-left: 20px;" class="fa fa-search"></i></h1>
+			Memudahkan anda dalam bertani
+		</div>
+		<div class="autentikasi">
+			<a href="masuk.php">Masuk</a>
+			<a href="daftar.php">Daftar</a> 
+		</div>
+		<nav>
+			<ul>
+				<li><a href="index.php">Beranda</a></li>
+				<li><a href="agri news.php">Diagnosa Penyakit</a></li>
+				<li><a href="agri news.php">Katalog Penyakit</a></li>
+			</ul>
+		</nav>
+	</div>
+</header>
